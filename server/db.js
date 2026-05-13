4abd16437cfa1fa23825bdccd352b12386d7c6ab
fnabilz <fikrizamzak@gmail.com>
2026-05-13 11:44:14 +0800

    part 3 complete

---
+++ server/db.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql2/promise') 
+  
+const pool = mysql.createPool({ 
+  host: 'localhost', 
+  port: 3306, 
+  user: 'root', 
+  password: '',              // Laragon default; do NOT do this in production 
+  database: 'student_records', 
+  waitForConnections: true, 
+  connectionLimit: 10, 
+  queueLimit: 0 
+}) 
+  
+module.exports = pool
